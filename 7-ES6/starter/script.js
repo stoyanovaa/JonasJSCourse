7444d691f807eeab588b0eaed8642f6999a4e681
Anna Stoyanova <anna.nikolova.stoqnowa@gmail.com>
2020-03-05 17:34:59 +0200

    es6-course

--- 7-ES6/starter/script.js
+++ 7-ES6/starter/script.js
@@ -1,13 +1,13 @@
-var Person = function(name, age){
+var Person = function(name, age) {
     this.name = name;
     this.age = age;;
 }
 
-Person.prototype.sleep = function(){
+Person.prototype.sleep = function() {
     console.log('Zzzz');
 }
 
-var Student = function(name, age, yearsOfStudy){
+var Student = function(name, age, yearsOfStudy) {
     // new Student will create empty {}
     // Person.call will add { this.name: name, this.age: age } to the object
     Person.call(this, name, age);
@@ -19,7 +19,7 @@ Student.prototype = Object.create(Person.prototype);
 // So now Student prototype.constructor is becoming Person's constructor. To fix this
 Student.prototype.constructor = Student;
 
-Student.prototype.study = function(){
+Student.prototype.study = function() {
     console.log('Study');
 }
 
@@ -28,13 +28,13 @@ s1.study();
 
 var p1 = new Person();
 /* Person should not be able to study. Child should not be able to change the behavior of the parent.
-*/
+ */
 p1.study(); //study is not a function as expected.
 
 //-------- 2 ------------//
 Student.prototype = Person.prototype;
 
-Student.prototype.party = function(){
+Student.prototype.party = function() {
     console.log('party!');
 }
 
@@ -42,7 +42,7 @@ var s2 = new Student();
 s2.party();
 
 var p2 = new Person();
- /* Person should not be able to party. Child should not be able to change the behavior of the parent.
+/* Person should not be able to party. Child should not be able to change the behavior of the parent.
 party function is defined and this isn't correct. Student.prototype = Person.prototype; will make Student.prototype reference
  be the same as Person.prototype reference so changes from Student.prototype update Person.prototype as well.
 */
@@ -70,34 +70,34 @@ HINT: Use some of the ES6 features: classes, subclasses, template strings,
  default parameters, maps, arrow functions, destructuring, etc.
 
 */
-class TownEntity  {
-    constructor(name, buildYear){
+class TownEntity {
+    constructor(name, buildYear) {
         this.name = name;
         this.buildYear = buildYear;
     }
 
 }
 
-class Park extends TownEntity{
-    constructor(name, buildYear, trees, area){
+class Park extends TownEntity {
+    constructor(name, buildYear, trees, area) {
         super(name, buildYear);
         this.trees = trees;
         this.area = area;
     }
 
-    density(){
-        return this.trees/this.area;
-    }
-    //Tree density of each park in the town (forumla: number of trees/park area)
+    density() {
+            return this.trees / this.area;
+        }
+        //Tree density of each park in the town (forumla: number of trees/park area)
 }
 
-class Street extends TownEntity{
-    constructor(name, buildYear, length){
+class Street extends TownEntity {
+    constructor(name, buildYear, length) {
         super(name, buildYear);
         this.length = length;
     }
 
-    classification(){
+    classification() {
         //Size classification of all streets: tiny/small/normal/big/huge
     }
 }
@@ -112,8 +112,7 @@ class Street extends TownEntity{
 5. Size classification of all streets: tiny/small/normal/big/huge. If the size is unknown, the default is normal
 
  */
-var parks = [new Park('p1',1900,12,12), new Park('p1',1910,12,12),new Park('p1',1920,120,56)];
+var parks = [new Park('p1', 1900, 12, 12), new Park('p1', 1910, 12, 12), new Park('p1', 1920, 120, 56)];
 var streets = [];
 
 let map = new Map();
-map.set
